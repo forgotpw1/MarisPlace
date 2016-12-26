39fb8dfef172798b474f7186ddd571448ccbdc1f
Barrett Griffith <mrbarrettgriffith@gmail.com>
2016-12-26 11:44:33 -0500

    Deploy Update

--- _script/install.sh
+++ _script/install.sh
@@ -4,7 +4,7 @@ set -x # Show the output of the following commands (useful for debugging)
 # Import the SSH deployment key
 ENCRYPTED_KEY_VAR="$encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="$encrypted_${ENCRYPTION_LABEL}_iv"
-openssl aes-256-cbc -K $encrypted_6a97e7e82599_key -iv $encrypted_6a97e7e82599_iv -in travis_deploy.enc -out travis_deploy -d
+openssl aes-256-cbc -K $encrypted_87e72e761fb2_key -iv $encrypted_87e72e761fb2_iv -in travis_deploy.enc -out travis_deploy -d
 rm travis_deploy.enc # Don't need it anymore
 chmod 600 travis_deploy
 #echo "Host samarashuttle.cr" >> ~/.ssh/config
